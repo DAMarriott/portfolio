7a985b8b2f86fdc496336a2645a1bc5cf4546265
DAMarriott <43251422+DAMarriott@users.noreply.github.com>
2019-12-19 13:11:07 -0800

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function menuEvent() {
+  document.getElementById("dropdown").classList.toggle("show");
 }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches(".menu-btn")) {
+    var dropdowns = document.getElementsByClassName("menu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
